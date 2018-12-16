75c68c6c3a2c39e7b505796f0db03a66d1579868
thomasvangoidsenhoven <thomas.vangoidsenhoven@student.ucll.be>
2018-12-16 13:31:01 +0100

    raytracer v3 compile fix + bindings

--- raytracer/raytracer/raytracers/ray-tracer-v3.cpp
+++ raytracer/raytracer/raytracers/ray-tracer-v3.cpp
@@ -23,21 +23,23 @@ imaging::Color raytracer::raytracers::_private_::RayTracerV3::process_light_ray(
 imaging::Color raytracer::raytracers::_private_::RayTracerV3::compute_specular(const MaterialProperties& material, const Hit& hit, const math::Ray& ray, const LightRay& lightray) const
 {
 	
-	Point3D rayOrigin = lightray.ray.origin;
-	Color rayColor = lightray.color;
+	const Point3D rayOrigin = lightray.ray.origin;
+	const Color rayColor = lightray.color;
 
-	Color specularColor = material.specular;
+	const Color specularColor = material.specular;
 
-	Point3D hitPosition = hit.position;
+	const Point3D hitPosition = hit.position;
 
-	Point3D eyePosition = ray.origin;
+	const Point3D eyePosition = ray.origin;
 
-	double specularExponent = material.specular_exponent;
+	 double specularExponent = material.specular_exponent;
 
-	Vector3D lightDirection = (hitPosition - eyePosition).normalized();
+	Vector3D lightDirection = (hitPosition - rayOrigin).normalized();
 	Vector3D reflectionDirection = lightDirection.reflect_by(hit.normal);
 	
-	double cos = ((ray.origin - hitPosition).normalized).dot(reflectionDirection);
+	Vector3D tussenstap = (eyePosition - hitPosition);
+	 tussenstap = tussenstap.normalized();
+	double cos = tussenstap.dot(reflectionDirection);
 	if (cos > 0) return rayColor * specularColor * pow(cos, specularExponent);
 	return colors::black();
 }
