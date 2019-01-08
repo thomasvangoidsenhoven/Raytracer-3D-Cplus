0a3b18a2f7101318196b560d066ed11cedb23d22
Thomas Vangoidsenhoven <vangoidsenhoventhomas@gmail.com>
2019-01-08 01:53:35 +0100

    able to laod all triangles not bounding boxes yet

--- raytracer/raytracer/primitives/mesh-reader.cpp
+++ raytracer/raytracer/primitives/mesh-reader.cpp
@@ -2,6 +2,7 @@
 #include "easylogging++.h"
 #include "primitives\primitives.h"
 #include "json/json.hpp"
+
 using namespace std;
 using nlohmann::json;
 //returns mesh primitive
@@ -17,11 +18,66 @@ Primitive raytracer::primitives::MeshReader::read_mesh(std::string path)
 		json json;
 		file >> json;
 
+		LOG(INFO) << "JSON SIZE" << json.size();
+		int count = 0;
+		// range-based for triangles
+		for (auto& element : json) {
+			if (!element["triangles"].is_null()) {
+				for (auto& triangle : element["triangles"]) {
+					count++;
+
+					auto puntA = triangle[0];
+					string aXs = puntA[0];
+					double aX;
+					stringstream(aXs) >> aX;
+					string aYs = puntA[1];
+					double aY;
+					stringstream(aYs) >> aY;
+					string aZs = puntA[2];
+					double aZ;
+					stringstream(aYs) << aZ;
+					Point3D a(aX,aY,aZ);
+					
+
+					auto puntB = triangle[0];
+					string bXs = puntB[0];
+					double bX;
+					stringstream(bXs) >> bX;
+					string bYs = puntB[1];
+					double bY;
+					stringstream(bYs) >> bY;
+					string bZs = puntB[2];
+					double bZ;
+					stringstream(bYs) << bZ;
+					Point3D b(bX, bY, bZ);
+
+
+					auto puntC = triangle[0];
+					string cXs = puntC[0];
+					double cX;
+					stringstream(cXs) >> cX;
+					string cYs = puntC[1];
+					double cY;
+					stringstream(cYs) >> cY;
+					string cZs = puntC[2];
+					double cZ;
+					stringstream(cZs) << cZ;
+					Point3D c(cX, cY, cZ);
+				
+					triangles.push_back(primitives::triangle(a, b, c));
+				}
+			}
+			
+		}
+
+		LOG(INFO) << "Amount of triangles: " << count;
+
+
 
 		//  crashes 3D studio string input = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 		//test create pyramid
 				//ay thats pretty good
-				Point3D p1 = Point3D::spherical(1, Angle::degrees(0),Angle::degrees(90));
+				/*Point3D p1 = Point3D::spherical(1, Angle::degrees(0),Angle::degrees(90));
 				Point3D p2 = Point3D::spherical(1, Angle::degrees(60), Angle::degrees(-30));
 				Point3D p3 = Point3D::spherical(1, Angle::degrees(180), Angle::degrees(-30));
 				Point3D p4 = Point3D::spherical(1, Angle::degrees(-60), Angle::degrees(-30));
@@ -32,7 +88,7 @@ Primitive raytracer::primitives::MeshReader::read_mesh(std::string path)
 				triangles.push_back(t1);
 				triangles.push_back(t2);
 				triangles.push_back(t3);
-				triangles.push_back(t4);
+				triangles.push_back(t4);*/
 	}
 	else {
 		LOG(INFO) << "Mission failed, We'll get em next time!";
